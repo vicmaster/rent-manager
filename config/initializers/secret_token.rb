24984a112ab672fef17c39f3200d51f706979a19
vicmaster <velazquezgaspar16@hotmail.com>
2013-02-26 15:52:09 -0600

    Rails app 2nd part

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentManager::Application.config.secret_token = '389170f450dc633ab4c28c05ff4d968af731113fbafd89b482c19bfff70aa1413554d5a8a4e99559036589e08a66452cf20b76f9457651916f11117800c4a235'
